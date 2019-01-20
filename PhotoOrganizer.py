073b86df615a64f536308d0986d71e0787b91ffe
OsamaTm <wooseokoh@naver.com>
2019-01-20 11:31:49 +0900

    1st Modified version. It works

--- PhotoOrganizer.py
+++ PhotoOrganizer.py
@@ -3,17 +3,62 @@ import time
 import datetime
 import shutil
 
-sourcedir = r'D:\InBox\0.Grouped\test'
-imagefiles = os.listdir(sourcedir)
+os.system('cls')
 
-for imagefile in imagefiles:
-    fileDate = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(sourcedir,imagefile)))
-    datePart = fileDate.date()
-    targetdir = datePart.isoformat()
+imagefiles = []
+Totalfiles = []
+NumOfFiles = 0
+i=0
 
-    if not(os.path.isdir(targetdir)):
-        os.makedirs(os.path.join(targetdir))
+sourceDirJpg = 'D:\\InBox\\1.JPG'
+sourceDirRAW = 'D:\\InBox\\2.RAW'
+targetPath = 'D:\\InBox\\0.Grouped'
 
-    shutil.move(os.path.join(sourcedir,imagefile),targetdir)
+print('\n',"Start JPG & MOV file Copy.",'\n')
 
+for path, folders, imagefiles in os.walk(sourceDirJpg):
+    Totalfiles += imagefiles
+NumOfFiles = len(Totalfiles)
 
+for path, folders, imagefiles in os.walk(sourceDirJpg):
+    for imagefile in imagefiles:
+        srcPath = os.path.join(path,imagefile)
+        fileDate = datetime.datetime.fromtimestamp(os.path.getmtime(srcPath))
+        datePart = fileDate.date()
+        targetdir = datePart.isoformat()
+
+        dstPath = os.path.join(targetPath,targetdir)
+
+        if not(os.path.isdir(dstPath)):
+            os.makedirs(dstPath)
+            print("Dest Folder <",dstPath,"> is created.")
+
+        shutil.copy2(srcPath,dstPath)
+        i+=1
+        print(imagefile," : ",i,"of",NumOfFiles,"JPG files are copied.")
+        
+i=0
+Totalfiles = []
+
+print('\n',"Start RAW file Copy.",'\n')
+
+for path, folders, imagefiles in os.walk(sourceDirRAW):
+    Totalfiles += imagefiles
+NumOfFiles = len(Totalfiles)
+
+for path, folders, imagefiles in os.walk(sourceDirRAW):
+    for imagefile in imagefiles:
+        srcPath = os.path.join(path,imagefile)
+        fileDate = datetime.datetime.fromtimestamp(os.path.getmtime(srcPath))
+        datePart = fileDate.date()
+        targetdir = datePart.isoformat()
+
+        dstPath = os.path.join(targetPath,targetdir,"RAW")
+
+        if not(os.path.isdir(dstPath)):
+            os.makedirs(dstPath)
+            print("Dest RAW Folder <",dstPath,"> is created.")
+
+        shutil.copy2(srcPath,dstPath)
+        i+=1
+        print(imagefile," : ",i,"of",NumOfFiles,"RAW files are copied.")
